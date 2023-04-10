23c8ddb41c77ac31d80befe0a7fa8446796fe21a
Dênis Augusto Luft <94466782+denisluft8@users.noreply.github.com>
2023-04-10 18:44:38 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import twitterLogo from "./assets/twitter-logo.svg";
+import "./App.css";
+
+
+const TWITTER_HANDLE = "web3dev_";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet encontrada!");
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            "Conectado com a Chave Pública:",
+            response.publicKey.toString()
+          );
+
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        alert("Objeto Solana não encontrado! Instale a Phantom Wallet 👻");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log(
+        "Conectado com a Chave Pública:",
+        response.publicKey.toString()
+      );
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Conecte sua carteira
+    </button>
+  );
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
+  return (
+    <div className="App">
+      <div className={walletAddress ? "authed-container" : "container"}>
+        <div className="header-container">
+          <p className="header">🖼Meu Portal de GIF</p>
+          <p className="sub-text">Veja sua coleção de GIF no metaverso ✨</p>
+          {!walletAddress && renderNotConnectedContainer()}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`feito por @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
